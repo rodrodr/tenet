d0ff4c3aaf82384acd6774f0d46d5eb108cb652a
Rodrigo Rodrigues-Silveira <rodrodr@gmail.com>
2023-04-29 15:16:36 +0200

    Add files via upload

--- R/colBright.R
+++ R/colBright.R
@@ -6,13 +6,11 @@
 # Dark colors would require white labels and light colors
 # would produce black labels
 #
-
-
-colBright <- function(colors, lim=130, bright="white", dark="black"){
+#' @export
+colBright <- function(colors, limit=130, bright="white", dark="black"){
   
-  require(grDevices)
-  
-  rgb <- col2rgb(colors)
+
+  rgb <- grDevices::col2rgb(colors)
   
   for(i in 1:ncol(rgb)){
     val <- 0.2126*rgb[1,i]+0.7152*rgb[2,i]+0.0722*rgb[3,i]
--- R/tfRatio.R
+++ R/tfRatio.R
@@ -1,19 +1,17 @@
-
-
-## 
-## 
-## This is the function tfRatio that calculates the term frequency
-## ratio in a series of documents.
-## 
-## 
-
+#
+#
+# This is the function tfRatio that calculates the term frequency
+# ratio in a series of documents.
+#
+#
+#' @export
 tfRatio <- function(text, keyword, threshold=0, return.selected=F){
-  
-  we <- stri_count(text, regex = keyword)
-  wt <- stri_count(text,regex = "\\w+")
-  
+
+  we <- stringi::stri_count(text, regex = keyword)
+  wt <- stringi::stri_count(text,regex = "\\w+")
+
   y<- round((we/wt*100)/(sum(we)/sum(wt)*100),2)
-  
+
   if(return.selection==F){
     return(y)
   }else{
