7e593052a2cb2cad74a68f0b4f1e5650d5ad7b59
Rodrigo Rodrigues-Silveira <rodrodr@gmail.com>
2023-04-30 23:21:57 +0200

    Add files via upload

--- R/tfRatio.R
+++ R/tfRatio.R
@@ -8,8 +8,8 @@
 tfRatio <- function(text, keyword, return.selected=FALSE, threshold=0){
 
   text <- stringi::stri_trans_general(text, "Latin-ASCII")
-  we <- stringi::stri_count(text, regex = keyword)
-  wt <- stringi::stri_count(text,regex = "\\w+")
+  we <- stringi::stri_count(text, regex = keyword, case_insensitive=T)
+  wt <- stringi::stri_count(text,regex = "\\w+", case_insensitive=T)
 
   y<- round((we/wt*100)/(sum(we)/sum(wt)*100),2)
 
