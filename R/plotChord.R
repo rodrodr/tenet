ebda8e3030c7a906298aca2710a563e5a104da8e
Rodrigo Rodrigues-Silveira <rodrodr@mac-papa.wifi.usal.es>
2023-06-06 14:08:57 +0200

    Commit from RStudio

--- R/plotChord.R
+++ R/plotChord.R
@@ -1,64 +1,15 @@
 #
 #
-# This is the function themeChord.
+# This is the function plotChord.
 #
 #' @import reshape2
 #' @import quanteda
 #' @import stringi
 #' @export
-themeChord <- function(corpus, dic, level=1, remove.self=TRUE, font.size=12, node.width=5, opacity=0.05, div.name="chordtheme", height=600, radius.percent=70, viewer=TRUE, html.return=FALSE, url.return=FALSE){
+plotChord <- function(data, from="from", to="to", value="value", font.size=12, node.width=5, opacity=0.05, div.name="chordtheme", height=600, radius.percent=70, viewer=TRUE, html.return=FALSE, url.return=FALSE){
   
   
-  fc <- quanteda::fcm(quanteda::tokens(corpus))
-  ff <- quanteda::fcm_select(fc, pattern = unlist(dic), valuetype = "regex")
-  
-  dm <- quanteda::convert(ff, "data.frame")
-  dm <- reshape2::melt(dm, id.vars = "doc_id")
-  dm <- dm[dm$value>0,]
-  dm$term1 <- NA
-  dm$term2 <- NA
-  
-  
-  if(level>1){
-    dic <- unlist(dic, recursive = F)
-    dic <- quanteda::dictionary(dic)
-    nm <- names(dic)
-    nma <- stringi::stri_split_fixed(nm, ".", 2, simplify = T)[,2]
-  }else{
-    nm <- names(dic)
-    nma <- nm  
-  }
-  
-  
-  for(i in 1:length(nm)){
-    
-    ky <- unlist(dic[nm[i]], use.names = F)
-    
-    for(k in 1:length(ky)){
-      
-      dm$term1[which(stringi::stri_detect_regex(dm$doc_id, pattern = ky[k], case_insensitive=T)==TRUE)] <- nma[i]
-      
-      dm$term2[which(stringi::stri_detect_regex(dm$variable, pattern = ky[k], case_insensitive=T)==TRUE)] <- nma[i]
-      
-      
-    }  
-    
-  }
-  
-  dm <- aggregate(list(value=dm$value), by=list(term1=dm$term1, term2=dm$term2), sum, na.rm=T)
-  
-  
-  dm <- aggregate(value~term1 + term2, data=transform(dm, term1 = pmin(term1, term2),term2 = pmax(term1, term2)), sum, na.rm = TRUE)
-  
-  dm <- dm[order(dm$term1, dm$term2),]
-  
-  if(remove.self==TRUE){
-    dm <- dm[dm$term1!=dm$term2,]
-  }
-  
-  dm$value <- round(dm$value/2)
-  
-  data <- paste0('{ "from": "', dm$term1, '", "to": "', dm$term2,'", "value":', dm$value, '}', collapse = ",\n")
+  data <- paste0('{ from: "', data[,from], '", to: "',data[,to],'", value: ', data[,value], '}', collapse = ',\n')
   
   
   partA <- paste0("<!DOCTYPE html>
