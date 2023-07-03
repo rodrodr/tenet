8604d7b65d104e0cac0170c16a54b56fe0cfbffa
Rodrigo Rodrigues-Silveira <rodrodr@Mac-Papa.local>
2023-07-03 21:20:34 +0200

    Commit from RStudio

--- R/forceDirectedTree.R
+++ R/forceDirectedTree.R
@@ -1,9 +1,12 @@
-#
-#
-# forceDirectedTree
-# 
+#' plotSankey
+#'
+#' Creates an amCharts Sankey Diagram
+#'
+#' @import htmlwidgets
+#' @import jsonlite
+#' 
 #' @export
-forceDirectedTree <- function(json_data, attraction= -5, collapsed=FALSE, palette="Spectral", col.n=9, show.link=TRUE, url.return=FALSE, html.return=FALSE, viewer=TRUE, height=800, max.radius=5, div.name="chartdiv"){
+forceDirectedTree <- function(json_data, attraction= -5, palette="Spectral", col.n=9, show.link=TRUE, height=800, max.radius=5, elementId="chartdiv"){
   
   max.radius <- as.character(max.radius)
   
@@ -11,139 +14,70 @@ forceDirectedTree <- function(json_data, attraction= -5, collapsed=FALSE, palett
   
   co <- paste0('am5.color("', co,'")',collapse = ",\n")
   
-  colla <- "" 
-  sl <- 1
-
+  colors <- htmlwidgets::JS(co)
+  
+  data <- paste0("{
+    value: 0,
+    children: [",json_data, "]}")
+  
+  opacity <- 1
+  
   if(show.link==F){
-    sl <- 0
+    opacity <- 0
   }
   
-  if(collapsed==T){
-    
-    colla <-   "
-    
-    // Start collapsed
-    networkSeries.maxLevels = 1;
-    
-    // Expand single level only
-    networkSeries.nodes.template.expandAll = false;"
-    
-  }
-  
-  partA <- paste0("<!DOCTYPE html>
-    <html>
-    <head>
-      <meta http-equiv='content-type' content='text/html; charset=UTF-8'>
-      <title>Force Directed Dendrogram</title>
-      <meta http-equiv='content-type' content='text/html; charset=UTF-8'>
-      <meta name='robots' content='noindex, nofollow'>
-      <meta name='googlebot' content='noindex, nofollow'>
-      <meta name='viewport' content='width=device-width, initial-scale=1'>
-    
-  
-  <style id='compiled-css' type='text/css'>
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-  }
-  
-  #",div.name,' {
-    width: 100%;
-    max-width: 100%;
-    height:',height,'px;
-  }
-    </style>
-  
-  </head>
-  <body>
-<script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-<script src="https://cdn.amcharts.com/lib/5/hierarchy.js"></script>
-<script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>  
 
-<div id="',div.name,'"></div>
   
-    <!-- TODO: Missing CoffeeScript 2 -->
+  # forward options using x
+  x = list(
+    data = data,
+    colors = colors,
+    opacity = opacity,
+    attraction = attraction,
+    max_radius = max.radius
+  )
   
-    <script type="text/javascript">//<![CDATA[
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'forceDirectedTree',
+    x,
+    width = "100%",
+    height = height,
+    package = 'tenet',
+    elementId = elementId
+  )
   
-      
-var root = am5.Root.new("',div.name,'");
+}
 
-// Set themes
-// https://www.amcharts.com/docs/v5/concepts/themes/
-root.setThemes([
-  am5themes_Animated.new(root)
-]);
 
-var data = {
-  value: 0,
-  children: [')
-  
-  
-  
-  partB <- paste0(']};
-                
-// Create wrapper container
-var container = root.container.children.push(am5.Container.new(root, {
-  width: am5.percent(100),
-  height: am5.percent(100),
-  layout: root.verticalLayout
-}));
+#' Shiny bindings for mywidget
+#'
+#' Output and render functions for using mywidget within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a mywidget
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name forceDirectedTree-shiny
+#'
+#' @export
+forceDirectedTreeOutput <- function(outputId, width = '100%', height = '800px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'forceDirectedTree', width, height, package = 'tenet')
+}
 
-var series = container.children.push(am5hierarchy.ForceDirected.new(root, {
-  singleBranchOnly: false,
-  downDepth: 2,
-  topDepth: 1,
-  initialDepth: 1,
-  valueField: "value",
-  categoryField: "name",
-  childDataField: "children",
-  idField: "name",
-  linkWithField: "linkWith",
-  manyBodyStrength: ', attraction, ',
-  minRadius: am5.percent(0.35),
-  maxRadius: am5.percent(', max.radius ,'),
-  centerStrength: 0.8
-}));
+#' @rdname forceDirectedTree-shiny
+#' @export
+renderforceDirectedTree <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, mywidgetOutput, env, quoted = TRUE)
+}
 
-series.get("colors").set("colors",[
-', co ,'
-]);
 
-series.links.template.set("strength", 0.9);
 
-series.links.template.set("strokeOpacity", ',sl,');
 
-series.data.setAll([data]);
-
-series.set("selectedDataItem", series.dataItems[0]);
-
-// Make stuff animate on load
-series.appear(1000, 100);
-
-</script>
-</body>
-</html>')
-  
-  
-  
-  ht <- paste(partA, json_data, partB, collapse = "\n")
-  
-  tp <- tempfile(pattern = "temp", fileext = ".html")
-  
-  write(ht, tp)
-  
-  
-  if(html.return==F & url.return==F){
-    if(viewer==T){
-      rstudioapi::viewer(tp)
-    }else{
-      utils::browseURL(tp)
-    }
-  }else if(html.return==F & url.return==T){
-    return(tp)
-  }else{
-    return(ht)
-  }
-  
-  
-}  
---
+++ inst/htmlwidgets/forceDirectedTree.js
@@ -0,0 +1,73 @@
+HTMLWidgets.widget({
+
+  name: 'forceDirectedTree',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+          var root = am5.Root.new(el.id);
+
+        // Set themes
+        // https://www.amcharts.com/docs/v5/concepts/themes/
+        root.setThemes([
+          am5themes_Animated.new(root)
+        ]);
+
+
+        var data = x.data;
+
+        // Create wrapper container
+        var container = root.container.children.push(am5.Container.new(root, {
+          width: am5.percent(100),
+          height: am5.percent(100),
+          layout: root.verticalLayout
+        }));
+        
+        var series = container.children.push(am5hierarchy.ForceDirected.new(root, {
+          singleBranchOnly: false,
+          downDepth: 3,
+          topDepth: 1,
+          initialDepth: 3,
+          valueField: "value",
+          categoryField: "name",
+          childDataField: "children",
+          idField: "name",
+          linkWithField: "linkWith",
+          manyBodyStrength: x.attraction,
+          minRadius: am5.percent(0.35),
+          maxRadius: am5.percent(x.max_radius),
+          centerStrength: 0.8
+        }));
+        
+        series.get("colors").set("colors",[x.colors]);
+        
+        series.links.template.set("strength", 0.9);
+        
+        series.links.template.set("strokeOpacity", x.opacity);
+        
+        series.data.setAll([data]);
+        
+        series.set("selectedDataItem", series.dataItems[0]);
+
+        // Make stuff animate on load
+        series.appear(1000, 100);
+
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
