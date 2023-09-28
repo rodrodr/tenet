9aee45674c4266aacb7bfed8756277f766d919e5
Rodrigo Rodrigues-Silveira <rodrodr@Mac-Papa.local>
2023-09-28 11:44:31 +0200

    Commit from RStudio

--- R/forceClusTree.R
+++ R/forceClusTree.R
@@ -5,34 +5,38 @@
 #' @import stringi
 #' @import quanteda
 #' @import quanteda.textstats
+#' @import htmlwidgets
 #' @export
 forceClusTree <- function(corpus, 
                            remove_punct=TRUE,
                            lang = "es",
-                           url.return = FALSE,
-                           html.return = FALSE,
-                           viewer = TRUE,
-                           palette = c("#DD8D29","#E2D200","#46ACC8","#E58601","#B40F20"),
+                           palette = c("#DD8D29",
+                                       "#E2D200",
+                                       "#46ACC8",
+                                       "#E58601",
+                                       "#B40F20"),
                            groupvar = NULL,
+                           width="100%",
                            height=580,
-                           link.col="#734421",
                            link.width=2,
-                           minRadius=0,
-                           maxRadius=100,
+                           maxRadius=30,
                            BodyStrength=-10,
                            include.text=FALSE,
                            img.docvar=NULL,
                            clust.method="euclidean",
-                           div.name="chartdivclus"){
+                           weight.scheme="logcount",
+                           elementId="chartdivclus"){
   
   
   # parte 1 - cluster
-  tk <- quanteda::tokens(corpus, remove_punct = remove_punct) |> 
+  tk <- quanteda::tokens(corpus, 
+                         remove_punct = remove_punct) |> 
     quanteda::tokens_remove(quanteda::stopwords(lang)) |> 
     quanteda::dfm() |> 
-    quanteda::dfm_trim(min_termfreq = 5, min_docfreq = 3)
+    quanteda::dfm_trim(min_termfreq = 5, 
+                       min_docfreq = 3)
   
-  tkd <- quanteda::dfm_weight(tk, scheme = "prop") %>%
+  tkd <- quanteda::dfm_weight(tk, scheme = weight.scheme) %>%
     quanteda.textstats::textstat_dist(method = clust.method) %>% 
     as.dist()
   
@@ -64,7 +68,8 @@ forceClusTree <- function(corpus,
       dx$text[i] <- paste(stringi::stri_wrap(dx$text[i], 60, 0.0, ), collapse = "<br>")
     }
     
-    text.node <- paste0('series.nodes.template.set(', '"tooltipHTML",','"',"<p><b>{name}:</b><br><br>{text}<br><br><img src='{poster}' width='300' height='450' style='display: block;margin-left: auto;margin-right: auto;'",'></p>");')
+    text.node <- "<p><b>{name}:</b><br><br>{text}<br><br><img src='{poster}' width='300' height='450' style='display: block;margin-left: auto;margin-right: auto;'></p>"
+    
   }else if (include.text==T & is.null(img.docvar)){
     
     dx$text <- as.character(corpus) 
@@ -74,18 +79,16 @@ forceClusTree <- function(corpus,
       dx$text[i] <- paste(stringi::stri_wrap(dx$text[i], 60, 0.0, ), collapse = "<br>")
     }
     
-    text.node <- paste0('series.nodes.template.set("tooltipHTML","<p><b>{name}:</b><br><br>{text}</p>);"')
+    text.node <- "<p><b>{name}:</b><br><br>{text}</p>);"
     
   }else if (include.text==F & ! is.null(img.docvar)){
     dx$text <- ""
-    text.node <- paste0('series.nodes.template.set(', '"tooltipHTML",','"',"<p><b>{name}:</b><br><br><img src='{poster}' width='300' height='450' style='display: block;margin-left: auto;margin-right: auto;'",'></p>");')
+    text.node <- "<p><b>{name}:</b><br><br><img src='{poster}' width='300' height='450' style='display: block;margin-left: auto;margin-right: auto;></p>"
     
   }else{
     dx$text <- ""
     
-    text.node <- 'series.nodes.template.set("tooltipHTML","<b>{name}:</b> {value}");'
-    
-    
+    text.node <- "<b>{name}:</b> {value}"
   }
   
   
@@ -135,130 +138,58 @@ forceClusTree <- function(corpus,
     value: 0,
     children: [', js, ']')
   
+  js <- paste0("{",  js, "}")
   
-  amcolor <- paste0(rep(paste0('am5.color("', link.col, '")'), nrow(dt)), collapse = ",\n")
+  data <- htmlwidgets::JS(js)
   
-  # parte 4 - crea el html
-  
-  partA <- paste0("<!DOCTYPE html>
-    <html>
-    <head>
-      <meta http-equiv='content-type' content='text/html; charset=UTF-8'>
-      <title>Force Directed Dendrogram</title>
-      <meta http-equiv='content-type' content='text/html; charset=UTF-8'>
-      <meta name='robots' content='noindex, nofollow'>
-      <meta name='googlebot' content='noindex, nofollow'>
-      <meta name='viewport' content='width=device-width, initial-scale=1'>
-    
-  
-  <style id='compiled-css' type='text/css'>
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-  }
-  
-  #", div.name, ' {
-    width: 100%;
-    height: ',height, 'px;
-  }
-  </style>
-    
-    </head>
-    <body>
-        <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-        <script src="https://cdn.amcharts.com/lib/5/hierarchy.js"></script>
-        <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
-        <div id="', div.name, '"></div>
-  
-    
-      <!-- TODO: Missing CoffeeScript 2 -->
-    
-      <script type="text/javascript">
-    
-          var root = am5.Root.new("', div.name,'");
-  
-          // Set themes
-          // https://www.amcharts.com/docs/v5/concepts/themes/
-          root.setThemes([
-            am5themes_Animated.new(root)
-          ]);
-  
-          var data = {')
-  
-  
-  partB <- paste0('};
-  
-  // Create wrapper container
-        var container = root.container.children.push(
-          am5.Container.new(root, {
-            width: am5.percent(100),
-            height: am5.percent(100),
-            layout: root.verticalLayout
-          })
-        );
-        
-        // Create series
-        // https://www.amcharts.com/docs/v5/charts/hierarchy/#Adding
-        var series = container.children.push(
-          am5hierarchy.ForceDirected.new(root, {
-            singleBranchOnly: false,
-            downDepth: 1,
-            topDepth: 1,
-            maxRadius: ', maxRadius,',
-            minRadius: ', minRadius,',
-            valueField: "value",
-            categoryField: "name",
-            childDataField: "children",
-            colorField: "color",
-            idField: "name",
-            linkWithStrength: 1,
-            linkWithField: "linkWith",
-            manyBodyStrength: ',BodyStrength,',
-            centerStrength: 0.5
-          })
-        );
-        
-        series.circles.template.setAll({
-            templateField: "nodeSettings"
-        });', text.node,'
-  
-        series.get("colors").set("colors", [', amcolor,']);
 
-        series.data.setAll([data]);
-        series.set("selectedDataItem", series.dataItems[0]);
-        
-        series.links.template.setAll({
-        colorMode: "solid",
-        fill: am5.color("#000000"),
-        strokeWidth: ',link.width, ',
-        strokeOpacity: 1,
-      });
+  # forward options using x
+  x = list(
+    data = data,
+    maxRadius = maxRadius,
+    tooltipText = text.node,
+    BodyStrength = BodyStrength,
+    linkWidth = link.width
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'forceClusTree',
+    x,
+    width = width,
+    height = height,
+    package = 'tenet',
+    elementId = elementId
+  )
   
-  
-        // Make stuff animate on load
-        series.appear(1000, 100);
-  
-      </script>
-  
-  </body>
-    </html>')
-  
-  ht <- paste(partA, js, partB, collapse = "\n")
-  
-  tp <- tempfile(pattern = "temp", fileext = ".html")
-  
-  write(ht, tp)
-  
-  if(html.return==F & url.return==F){
-    if(viewer==T){
-      rstudioapi::viewer(tp)
-    }else{
-      browseURL(tp)
-    }
-  }else if(html.return==F & url.return==T){
-    return(tp)
-  }else{
-    return(ht)
-  }
-  
+}
+
+
+#' Shiny bindings for mywidget
+#'
+#' Output and render functions for using mywidget within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a mywidget
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name forceClusTree-shiny
+#'
+#' @export
+forceClusTreeOutput <- function(outputId, width = '100%', height = '800px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'forceClusTree', width, height, package = 'tenet')
+}
+
+#' @rdname forceClusTree-shiny
+#' @export
+renderforceClusTree <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, forceClusTreeOutput, env, quoted = TRUE)
 }
 
---
+++ inst/htmlwidgets/forceClusTree.js
@@ -0,0 +1,81 @@
+HTMLWidgets.widget({
+
+  name: 'forceClusTree',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function(x) {
+
+          var root = am5.Root.new(el.id);
+
+        // Set themes
+        // https://www.amcharts.com/docs/v5/concepts/themes/
+        root.setThemes([
+          am5themes_Animated.new(root)
+        ]);
+
+
+        var data = x.data;
+
+        // Create wrapper container
+        var container = root.container.children.push(am5.Container.new(root, {
+          width: am5.percent(100),
+          height: am5.percent(100),
+          layout: root.verticalLayout
+        }));
+        
+        var series = container.children.push(am5hierarchy.ForceDirected.new(root, {
+          singleBranchOnly: false,
+          downDepth: 2,
+          topDepth: 1,
+          initialDepth: 4,
+          valueField: "value",
+          categoryField: "name",
+          childDataField: "children",
+          idField: "name",
+          colorField: "color",
+          linkWithField: "linkWith",
+          manyBodyStrength: x.BodyStrength,
+          minRadius: am5.percent(0),
+          maxRadius: am5.percent(x.maxRadius),
+          centerStrength: 0.5
+        }));
+        
+         series.circles.template.setAll({
+            templateField: "nodeSettings"
+        });
+        
+        series.nodes.template.set("tooltipHTML",x.tooltipText);
+        
+        series.data.setAll([data]);
+    
+        series.set("selectedDataItem", series.dataItems[0]);
+
+  
+        series.links.template.setAll({
+        colorMode: "solid",
+        strokeWidth: linkWidth,
+        strokeOpacity: 1
+      });
+
+        // Make stuff animate on load
+        series.appear(1000, 100);
+
+
+      },
+
+      resize: function(width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
