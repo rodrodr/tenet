d76b28bf24cf5898088f191a69bdf3a45ac74eb6
Rodrigo Rodrigues-Silveira <rodrodr@MacBook-Pro.local>
2023-12-20 21:53:39 +0100

    Commit from RStudio

--- R/tagCorpus.R
+++ R/tagCorpus.R
@@ -18,51 +18,75 @@ tagCorpus <- function(corpus,
                       bright = 130, 
                       pagination = TRUE, 
                       defaultPageSize=10,
-                      show.details=TRUE){
+                      show.details=TRUE,
+                      case.insensitive=TRUE,
+                      remove.accent=TRUE,
+                      filter=FALSE,
+                      data.return=FALSE,
+                      quietly=TRUE){
+  
+  nm <- names(dic)
+  
+  if(quietly==FALSE){
+    pb <- utils::txtProgressBar(min = 0, max=length(nm), style=3, char="=", width = 50)
+  }
   
   if(reshape.dic==TRUE){
     cp <- quanteda::corpus_reshape(corpus, reshape.to)
   }
   
   docs <- docnames(cp)
+  
   docs <- stringi::stri_split_fixed(docs, pattern = ".",2, simplify = T)[,1]
   
   text <- as.character(cp)
   
-  text <- stringi::stri_trans_general(text, "Latin-ASCII")
-  
-  nm <- names(dic)
+  if (remove.accent==TRUE){
+    text <- stringi::stri_trans_general(text, "Latin-ASCII")
+  }
   
   col <- selColors(palette=palette, col.n=length(nm))
-
+  
   tcol <- colBright(col, limit = bright)
   
+  base <- paste0("<span style='color:", 
+                 tcol,";background-color:",
+                 col,";padding:5px;border-radius:5px;margin-top:3px;margin-left:3px;display:inline-block;'>")
+  
+  
   dc <- data.frame()
+  
   
   for(i in 1:length(nm)){
     
     ka <- as.character(unlist(dic[i]))  
-    ka <- stringi::stri_trans_general(ka, "Latin-ASCII")
     
-    for(k in 1:length(ka)){
-      
-      bb <- paste0("\\b(", ka[k], "(?:[^\\s]|(?!\\w)){1,})\\b")
-      bx <- paste0("<span style='color:", 
-                   tcol[i], "; background-color:", 
-                   col[i], "'>", "$1", "</span>")    
-      
-      freq <- stringi::stri_count_regex(text, pattern = bb, case_insensitive=T)
-      
-      dc<- rbind(dc, data.frame(cat=nm[i], 
-                                order=c(1:length(text)),
-                                freq=freq))
-      
-      text <- stringi::stri_replace_all_regex(text, 
-                                              pattern = bb, 
-                                              replacement = bx,
-                                              case_insensitive = T)   
-    }             
-  }
+    if (remove.accent==TRUE){
+      ka <- stringi::stri_trans_general(ka, "Latin-ASCII")
+    }
+    
+    bb <- paste0("\\b((", paste(ka,collapse = "|"), ")(?:[^\\s]|(?!\\w)){1,})\\b")
+    bx <- paste0("<span style='color:", 
+                 tcol[i], "; background-color:", 
+                 col[i], "'>", "$1", "</span>")    
+    
+    freq <- stringi::stri_count_regex(text, pattern = bb, case_insensitive=T)
+    
+    text <- stringi::stri_replace_all_regex(text, pattern = bb, replacement = bx,
+                                            opts_regex = stringi::stri_opts_regex(
+                                              case_insensitive = case.insensitive), vectorize_all = F)
+    
+    dc<- rbind(dc, data.frame(cat=nm[i], 
+                              order=c(1:length(text)),
+                              freq=freq))
+    
+    dc <- dc[dc$freq>0,]
+    
+    if(quietly==FALSE){
+      utils::setTxtProgressBar(pb, value = i)
+    }
+    
+  }      
   
   da <- aggregate(list(matches=dc$freq), 
                   by=list(order=dc$order), 
@@ -72,10 +96,9 @@ tagCorpus <- function(corpus,
                   by=list(order=dc$order, cat=dc$cat), 
                   sum, na.rm=T)
   
-  df <- db[db$matches>0,]
-  db <- db[db$matches>0,]
+  df <- db
   
-  dc <- aggregate(list(ncats=db$order), 
+  dx <- aggregate(list(ncats=db$order), 
                   by=list(order=db$order), length)
   
   dm <- aggregate(list(max=df$matches), 
@@ -84,21 +107,19 @@ tagCorpus <- function(corpus,
   df <- merge(df, dm, by="order")
   df <- df[df$matches==df$max,]
   
-  base <- paste0("<span style='color:", 
-                 tcol,";background-color:",
-                 col,";padding:5px;border-radius:5px;margin-top:3px;margin-left:3px;display:inline-block;'>")
+  df$main_cat_span <- NA
+  db$cats_span <- NA
   
-  df$main_cat <- NA
-  db$cats <- NA
   for(i in 1:length(nm)){
-    df$main_cat[which(df$cat==nm[i])] <- paste0(base[i],df$cat[which(df$cat==nm[i])] ,"</span>") 
-    db$cats[which(db$cat==nm[i])] <- paste0(base[i],db$cat[which(db$cat==nm[i])] ,"</span>") 
+    df$main_cat_span[which(df$cat==nm[i])] <- paste0(base[i],df$cat[which(df$cat==nm[i])] ,"</span>") 
+    db$cats_span[which(db$cat==nm[i])] <- paste0(base[i],db$cat[which(db$cat==nm[i])] ,"</span>") 
     
   }
   
-  df <- aggregate(list(main_cat=df$main_cat), 
+  
+  df <- aggregate(list(main_cat=df$cat, main_cat_span=df$main_cat_span), 
                   by=list(order=df$order), paste0, collapse=" ")
-  db <- aggregate(list(cats=db$cats), 
+  db <- aggregate(list(cats=db$cat, cats_span=db$cats_span), 
                   by=list(order=db$order), paste0, collapse=" ")
   
   data <- data.frame(docid=docs, order=1:length(text), text)
@@ -106,42 +127,62 @@ tagCorpus <- function(corpus,
   data <- merge(data, df, by="order", all.x=T)
   data <- merge(data, db, by="order", all.x=T)
   data <- merge(data, da, by="order", all.x=T)
-  data <- merge(data, dc, by="order", all.x=T)
+  data <- merge(data, dx, by="order", all.x=T)
   
   data$ncats[is.na(data$ncats)]<-0
+  data$matches[is.na(data$matches)]<-0
   
-  data$order <- ave(data$order, data$docid, FUN=seq_along)
   
+  if(filter==TRUE) data <- data[data$ncats>0,]
   
-  reactable::reactable(data,
-                       resizable = TRUE,
-                       wrap=TRUE,
-                       pagination = pagination,
-                       defaultPageSize = defaultPageSize,
-                       columns = list(
-                         docid=colDef(name="Doc.",
-                                      filterable = T,
-                                      width = 120, 
-                                      show = show.details),
-                         order=colDef(name="Order",
-                                      width = 80, 
-                                      show = show.details),
-                         text=colDef(name="Paragraph",
-                                     filterable = T,
-                                     html = T),
-                         main_cat=colDef(name="Main Category",
-                                         filterable = T,
-                                         html=T, width=140),
-                         cats=colDef(name="All Categories",
-                                     filterable = T,
-                                     html=T, width=140),
-                         matches=colDef(name="Matches",
-                                        filterable = T,
-                                        html=T, width=100, 
-                                        show = show.details),
-                         ncats=colDef(name="Cat. No.",
-                                      filterable = T,
-                                      html=T, width=80, 
-                                      show = show.details)))
+  if(data.return==TRUE){
     
+    data$main_cat_span <- NULL
+    data$cats_span <- NULL
+    
+    return(data)
+    
+  }else{
+    
+    data$main_cat <- data$main_cat_span
+    data$cats <- data$cats_span
+    
+    data$main_cat_span <- NULL
+    data$cats_span <- NULL
+    
+    data$order <- ave(data$order, data$docid, FUN=seq_along)
+    
+    reactable::reactable(data,
+                         resizable = TRUE,
+                         wrap=TRUE,
+                         pagination = pagination,
+                         defaultPageSize = defaultPageSize,
+                         columns = list(
+                           docid=reactable::colDef(name="Doc.",
+                                                   filterable = T,
+                                                   width = 120, 
+                                                   show = show.details),
+                           order=reactable::colDef(name="Order",
+                                                   width = 80, 
+                                                   show = show.details),
+                           text=reactable::colDef(name="Paragraph",
+                                                  filterable = T,
+                                                  html = T),
+                           main_cat=reactable::colDef(name="Main Category",
+                                                      filterable = T,
+                                                      html=T, width=140),
+                           cats=reactable::colDef(name="All Categories",
+                                                  filterable = T,
+                                                  html=T, width=140),
+                           matches=reactable::colDef(name="Matches",
+                                                     filterable = T,
+                                                     html=T, width=100, 
+                                                     show = show.details),
+                           ncats=reactable::colDef(name="Cat. No.",
+                                                   filterable = T,
+                                                   html=T, width=80, 
+                                                   show = show.details)))
+  }
+  
 }
+
