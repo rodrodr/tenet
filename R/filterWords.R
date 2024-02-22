66937be3f0b21c209a5231fce8efc4666775e2aa
Rodrigo Rodrigues-Silveira <rodrodr@MacBook-Pro.local>
2024-02-22 11:28:16 +0100

    Commit from RStudio

--- R/filterWords.R
+++ R/filterWords.R
@@ -12,54 +12,54 @@
 #' @export
 filterWords <- function(corpus,
                         keywords,
-                        rem.accent = TRUE,
+                        rem.accent = FALSE,
                         rem.punct = TRUE,
                         case.insensitive = TRUE,
-                        lang = "es"){
+                        lang = "es",
+                        fast = TRUE){
   
   group <- NULL
   
-  if(quanteda::is.dictionary(keywords)){
-    
+  if (quanteda::is.dictionary(keywords)) {
     nm <- names(keywords)
-    
     kw <- vector()
-    
-    for(i in 1:length(keywords)){
+    for (i in 1:length(keywords)) {
       kw <- c(kw, keywords[[i]])
       group <- c(group, rep(nm[i], length(keywords[[i]])))
     }
-    
     keywords <- kw
   }
   
-  dt <- data.frame()
   
-  dt <- quanteda::tokens(as.character(corpus))
+  if(fast==TRUE){
+    dt <-outer(as.character(corpus), "\\s+", 
+               FUN = stringi::stri_split_regex)
+    nm <- rep(row.names(dt), lengths(dt))
+  }else{
+    dt <- quanteda::tokens(as.character(corpus), remove_punct = rem.punct)
+    nm <- rep(names(lengths(dt)), lengths(dt))
+  }
   
-  nm <- rep(names(lengths(dt)),lengths(dt))
+  
   wd <- unlist(dt)
+  dt <- data.frame(name = nm, word = wd)
+  dt <- dt[!tolower(dt$word) %in% stopwords(lang), ]
+  dt <- dt[which(dt$word != " "), ]
+  dt <- dt[which(dt$word != ""), ]
+  dt <- dt[which(dt$word != "\n"), ]
   
-  dt <- data.frame(name = nm, 
-                   word = wd)
-  
-  if(rem.accent==T){
+  if (rem.accent == T) {
     dt$word <- stringi::stri_trans_general(dt$word, "Latin-ASCII")
-  } 
-  if(rem.punct==T){
-    dt$word <- stringi::stri_replace_all(dt$word, regex = "[:punct:]", "")
-  } 
+    keywords <- stringi::stri_trans_general(keywords, "Latin-ASCII")
+    
+  }
   
-  dt <- dt[which(dt$word!=" "),]
-  dt <- dt[which(dt$word!=""),]
-  dt <- dt[which(dt$word!="\n"),]
+  if (rem.punct == T) {
+    dt$word <- stringi::stri_replace_all(dt$word, regex = "[:punct:]", 
+                                         "")
+  }
   
-  
-  dt <- dt |> 
-    dplyr::group_by(.data$name) |> 
-    dplyr::mutate(index = dplyr::row_number()/dplyr::n())
-  
-  dt <- dt[!tolower(dt$word) %in% stopwords(lang),]
+  dt <- dplyr::mutate(dplyr::group_by(dt, .data$name), index = dplyr::row_number()/dplyr::n())
   
   keywords <- paste0("^", keywords)
   
